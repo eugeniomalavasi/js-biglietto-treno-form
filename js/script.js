8ba6a584d0e83b143476196b5d5212d3e8637fa3
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-26 16:23:23 +0100

    feat: input values gatherd with variables at the mouse click

--- js/script.js
+++ js/script.js
@@ -7,15 +7,18 @@ const sendBtn = document.getElementById("send_button");
 // eventlistener del bottone
 sendBtn.addEventListener ("click", function() {
 console.log("click");
-// selezione dell'input e trasformazione in numero
-let userKm = parseInt(document.getElementById("user_km")); // number
+// selezione dell'input km
+const userKm = document.getElementById("user_km"); // object
 console.log(userKm , typeof userKm);
-
-let userAge = parseInt(document.getElementById("age")); // number
+// selezione dell'input Age
+const userAge = document.getElementById("age"); // object
 console.log(userAge , typeof userAge);
-
-const inputKm = userKm;
-console.log(inputKm, typeof inputKm);
+// raccolta input km
+const kmValue = parseInt(userKm.value); // number
+console.log(kmValue, typeof kmValue);
+// raccolta input age
+const ageValue = parseInt(userAge.value) // number
+console.log(ageValue, typeof ageValue);
 })
 
 // LOGICA
