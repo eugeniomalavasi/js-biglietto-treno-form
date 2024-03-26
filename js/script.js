67ef4d728dc0633a57968db2b5d2938cb34a3295
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-26 16:58:13 +0100

    feat: mileston 1 complete

--- js/script.js
+++ js/script.js
@@ -13,14 +13,38 @@ console.log(userKm , typeof userKm);
 // selezione dell'input Age
 const userAge = document.getElementById("age"); // object
 console.log(userAge , typeof userAge);
+
 // raccolta input km
 const kmValue = parseInt(userKm.value); // number
 console.log(kmValue, typeof kmValue);
 // raccolta input age
 const ageValue = parseInt(userAge.value) // number
 console.log(ageValue, typeof ageValue);
-})
+
+// prezzo al km
+const priceKm = 0.21; // number
+
+// calcolo costo biglietto base
+let baseTicket = priceKm * kmValue; // number
+console.log(baseTicket, "prezzo biglietto base");
 
 // LOGICA
+// calcolo totale sconto
+let discount = 0;
+
+if (ageValue < 18) {
+    discount = 20;
+} else if (ageValue > 65) {
+    discount = 40;
+} 
+
+let totalDiscount = (baseTicket * discount) / 100;
+console.log(totalDiscount, "ammontare dello sconto");
+
+let ticketPrice = baseTicket - totalDiscount;
+console.log(ticketPrice, "prezzo finale del biglietto");
+})
+
+
 
 // OUTPUT
